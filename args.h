87b91f0e568dc01533fe23a627504292196a85b5
Michal Pokorný <pok@rny.cz>
2015-03-27 15:52:17 +0100

    Add some documentation

--- args.h
+++ args.h
@@ -1,30 +1,65 @@
+// Command-line argument parsing library
+//
+// Pavel Pilař, Michael Pokorný
+//
+// The API is deliberately designed to be very simple for the user.
+// The user only needs to register options via AddBool/Int/String/Enum and call
+// Parse in main().
+//
+// Strictly speaking, the assignment asked to implement "options that aren't
+// allowed to have a value". This makes sense with booleans, but since it would
+// be impossible to pass a non-bool option without a value, this makes no
+// sense for non-bools. Thus we decided to skip this part of the assignment.
+//
+// TODO: But it might make sense to only allow "-x" for booleans, while
+// not allowing "-x false"/"-x=true". Perhaps we should add this as a special
+// mode for AddBool?
+// TODO: is it a "flag", or an "option"? unify nomenclature
+
 #include <string>
 #include <vector>
 #include <utility>
 
+// All functions and types of the library are within the args namespace.
 namespace args {
-
-// Preskoceno: "zakazane hodnoty optionu" pro string flagy, int flagy
-// (jinde nedava smysl).
 
 // TODO: "ls -laxHrtlm"
 
-// int main(char** argv, int argc) {
-//   flags::Parse(&argv, &argc);
-//   // -> argv, argc jsou argumenty co nejsou options
-// }
+// Prints out help about all registered options to standard output.
+void Usage() {
+}
+
+// Parse gets a pointer to argc and argv as given to main(), parses out
+// options and modifies argc and argv to contain only nonoption arguments.
+// If option parsing fails, Parse prints out an error message describing
+// the problem and exits with exit code 1.
+// If no long option named "--help" or short option named "-h" is registered,
+// passing either of these options calls Usage and exits with exit code 0.
+//
+// This method doesn't throw exceptions or indicate failure via a return value
+// to make the most common use case simpler (most programs don't need anything
+// but the default behavior).
+//
+// Intended usage:
+//   int main(int argc, char** argv) {
+//     flags::Parse(&argc, &argv);
+//     // argc, argv now only contain non-option arguments.
+//   }
 void Parse(int* argc, char*** argv) {
 }
 
+// TODO: Should this display help and quit on --help?
+// TODO: Is it OK to just say "flags OK" / "flags not OK"?
 bool TryParse(int argc, const char** argv) {
-}
-
-void Usage() {
 }
 
 const std::vector<std::string>& GetNonoption() {
   return *(new std::vector<std::string>);
 }
+
+// The args::internal namespace contains implementation details that need
+// to be visible in the header due to C++ rules.
+namespace internal {
 
 struct FlagName {
  public:
@@ -64,6 +99,8 @@ class Flag {
   // TODO: mozna explicitni set?
 };
 
+// TODO: BoolFlag, IntFlag
+
 class StringFlag : public Flag<std::string> {
  public:
   std::string get() override {
@@ -74,47 +111,62 @@ class StringFlag : public Flag<std::string> {
   }
 };
 
-class EnumFlag : public StringFlag {
- public:
-  bool value_allowed(const std::string&) {
-    return false;
-  }
-};
+class EnumFlag : public StringFlag {};
 
-// Publicly exposed name: args::String, args::Enum.
-typedef StringFlag String;
-typedef EnumFlag Enum;
+}  // namespace internal
+
+// In the context of the library, flags are represented as internal::Flag
+// subclasses (internal::BoolFlag, internal::IntFlag, ...).
+// These internal names are exposed as args::Bool, args::Int, ... to make usage
+// simpler (e.g. "args::String my_flag" instead of "args::StringFlag my_flag").
+typedef internal::StringFlag String;
+typedef internal::EnumFlag Enum;
 
 // struct {
-//   flags::StringFlag my_string;
+//   args::StringFlag my_string;
 // } Options;
 //
-// flags::AddString(&Options.my_string, "output", REQUIRED, "this flag is useful");
+// args::AddString(&Options.my_string, "output", args::REQUIRED, "this flag is useful");
 // Options.my_string.alias('o', "output_thing")
-// flags::Alias(&Options.my_string, 'o', "output_thing");
+// args::Alias(&Options.my_string, 'o', "output_thing");
 const bool REQUIRED = true;
 const bool OPTIONAL = false;
-StringFlag& AddString(StringFlag*, const FlagName& name, bool required, const std::string& documentation) {
-  return *(new StringFlag);
+internal::StringFlag& AddString(internal::StringFlag*,
+                                const internal::FlagName& name,
+                                bool required,
+                                const std::string& documentation) {
+  return *(new internal::StringFlag);
 }
 
 // TODO: namapovat enum na C++ enum?
 
-// TODO: template... ...
-StringFlag& Alias(StringFlag*, const FlagName& name) {
+// TODO: Alias should be either templated over Flag derived classes, or
+// copied for all flag types.
+// OR - we could also make it 'void Alias(Flag*, FlagName),', which would
+// limit the fluentness of the API. I think it would be a good idea.
+// Less complexity that way.
+internal::StringFlag& Alias(internal::StringFlag*,
+                            const internal::FlagName& name) {
 }
-StringFlag& Alias(StringFlag*, const std::initializer_list<FlagName>& names) {
+internal::StringFlag& Alias(internal::StringFlag*,
+                            const std::initializer_list<internal::FlagName>&
+                                names) {
 }
 
-EnumFlag& AddEnum(EnumFlag*, const FlagName& name, bool required, const std::initializer_list<std::string>& allowed_values,
-		  const std::string& documentation) {
-  return *(new EnumFlag);
+internal::EnumFlag& AddEnum(internal::EnumFlag*,
+                            const internal::FlagName& name, bool required,
+                            const std::initializer_list<std::string>&
+                                allowed_values,
+		            const std::string& documentation) {
+  return *(new internal::EnumFlag);
 }
-EnumFlag& AddEnum(EnumFlag*, const FlagName& name, bool required, std::initializer_list<const char*> allowed_values,
-		  const std::string& documentation) {
-  return *(new EnumFlag);
+internal::EnumFlag& AddEnum(internal::EnumFlag*,
+                            const internal::FlagName& name, bool required,
+                            std::initializer_list<const char*> allowed_values,
+		            const std::string& documentation) {
+  return *(new internal::EnumFlag);
 }
 
 // TODO: boolean, int
 
-}
+}  // namespace args
