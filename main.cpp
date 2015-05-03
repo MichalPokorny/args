a29e25b388187f2972da07af6c09d39958f6b174
unknown <pilar.pavel.pili@gmail.com>
2015-05-03 16:09:13 +0200

    fix pull request notes

--- main.cpp
+++ main.cpp
@@ -14,38 +14,39 @@ args::Bool verbose;
 
 int main(int argc, char** argv) {
   // Bind our variables to new flags.
-  args::AddBool(&verbose, {'v', "verbose"}, args::OPTIONAL,
+  args::AddBool(&verbose, {'v', "verbose"}, args::FlagUse::OPTIONAL,
                 "Enable verbose logging");
-
-  args::AddInt(&size, {'s', "size"}, args::OPTIONAL,
+  args::AddInt(&size, {'s', "size"}, args::FlagUse::REQUIRED,
                "Size of something");
 
   // Parse command line flags. Die on error.
   args::Parse(&argc, &argv);
   
-  if (verbose.present()){
-    printf("verbose = true");
+  // Check if "verbose" flag is present and true.
+  if (verbose.present() && verbose.get()) {
+    std::cout << "verbose = true" << std::endl;
   } else {
-    printf("verbose = false");
+    std::cout << "verbose = false" << std::endl;
   }
 
-  if (size.present()){
-    printf("size = %i", size.get());
+  // Check if "size" flag is present. If so get its value.
+  if (size.present()) {
+    std::cout << "size = " <<  size.get() << std::endl;
   } else {
-    printf("size = 42");
+    std::cout << "size = 42" << std::endl;
   }
   
   // argc, argv now point to the original zeroth argument
   // (usually the program name) and nonflags arguments.
   std::vector<char*> non_flags(argc);
-  copy(&argv[1], &argv[argc], non_flags.begin()); //skip 0
+  copy(&argv[1], &argv[argc], non_flags.begin()); // skip [0]
 
-  //print non flags arguments
-  cout << "args =";
-  for (auto non_flag : non_flags){
-    cout << non_flag << " ";
+  // Print non flags arguments.
+  std::cout << "args =";
+  for (const auto& non_flag : non_flags) {
+    std::cout << non_flag << " ";
   }
-  cout << endl;
+  std::cout << std::endl;
 
   return 0;
 }
